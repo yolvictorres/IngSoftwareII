30ab00f60e8f5a0d92bf8009a2a7dce26c1f038c
ian <sebas-9523@hotmail.com>
2017-08-14 16:18:35 -0500

    cambio de nombres en las clases usuario por persona y publicador por empresa

---
+++ Camello/src/java/Modelo/Empresa.java
@@ -0,0 +1,73 @@
+package Modelo;
+
+public class Empresa {
+
+    private int idEmpresa;
+    private String nombreEmpresa;
+    private String detallesEmpresa;
+    private String correoEmpresa;
+    private String campoLaboral;
+    private String rutaLogo;
+
+    public Empresa() {
+    }
+
+    public Empresa(int idEmpresa, String nombreEmpresa, String detallesEmpresa, String correoEmpresa, String campoLaboral) {
+        this.idEmpresa = idEmpresa;
+        this.nombreEmpresa = nombreEmpresa;
+        this.detallesEmpresa = detallesEmpresa;
+        this.correoEmpresa = correoEmpresa;
+        this.campoLaboral = campoLaboral;
+    }
+
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDetallesEmpresa() {
+        return detallesEmpresa;
+    }
+
+    public void setDetallesEmpresa(String detallesEmpresa) {
+        this.detallesEmpresa = detallesEmpresa;
+    }
+
+    public String getCorreoEmpresa() {
+        return correoEmpresa;
+    }
+
+    public void setCorreoEmpresa(String correoEmpresa) {
+        this.correoEmpresa = correoEmpresa;
+    }
+
+    public String getCampoLaboral() {
+        return campoLaboral;
+    }
+
+    public void setCampoLaboral(String campoLaboral) {
+        this.campoLaboral = campoLaboral;
+    }
+
+    public String getRutaLogo() {
+        return rutaLogo;
+    }
+
+    public void setRutaLogo(String rutaLogo) {
+        this.rutaLogo = rutaLogo;
+    }
+
+
+
+}
---
+++ Camello/src/java/Modelo/Persona.java
@@ -0,0 +1,112 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Persona {
+    
+    private int idPersona;
+    private String nombresPersona;
+    private String apellidosPersona;
+    private int telefono;
+    private int edad;
+    private String correoPersona;
+    private String direccionPersona;
+    private ArrayList<String> interesLaboral;
+    private String rutaFoto;
+    private String rutaHojadevida;
+
+    public Persona(int idPersona, String nombresPersona, String apellidosPersona, int telefono, int edad, String correoPersona, String direccionPersona, ArrayList<String> interesLaboral) {
+        this.idPersona = idPersona;
+        this.nombresPersona = nombresPersona;
+        this.apellidosPersona = apellidosPersona;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.correoPersona = correoPersona;
+        this.direccionPersona = direccionPersona;
+        this.interesLaboral = interesLaboral;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombresPersona() {
+        return nombresPersona;
+    }
+
+    public void setNombresPersona(String nombresPersona) {
+        this.nombresPersona = nombresPersona;
+    }
+
+    public String getApellidosPersona() {
+        return apellidosPersona;
+    }
+
+    public void setApellidosPersona(String apellidosPersona) {
+        this.apellidosPersona = apellidosPersona;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreoPersona() {
+        return correoPersona;
+    }
+
+    public void setCorreoPersona(String correoPersona) {
+        this.correoPersona = correoPersona;
+    }
+
+    public String getDireccionPersona() {
+        return direccionPersona;
+    }
+
+    public void setDireccionPersona(String direccionPersona) {
+        this.direccionPersona = direccionPersona;
+    }
+
+    public ArrayList<String> getInteresLaboral() {
+        return interesLaboral;
+    }
+
+    public void setInteresLaboral(ArrayList<String> interesLaboral) {
+        this.interesLaboral = interesLaboral;
+    }
+
+    public String getRutaFoto() {
+        return rutaFoto;
+    }
+
+    public void setRutaFoto(String rutaFoto) {
+        this.rutaFoto = rutaFoto;
+    }
+
+    public String getRutaHojadevida() {
+        return rutaHojadevida;
+    }
+
+    public void setRutaHojadevida(String rutaHojadevida) {
+        this.rutaHojadevida = rutaHojadevida;
+    }
+
+   
+    
+}
+
