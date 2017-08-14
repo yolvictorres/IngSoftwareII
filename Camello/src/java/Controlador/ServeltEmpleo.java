554688ba0b6ba7517dbf70a351945894ef1e3309
ian <sebas-9523@hotmail.com>
2017-08-14 16:40:27 -0500

    cambio en servlet del paquete Controlador

--- Camello/src/java/Controlador/ServeltEmpleo.java
+++ Camello/src/java/Controlador/ServeltEmpleo.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author usuario
  */
-public class ServletEmpresa extends HttpServlet {
+public class ServeltEmpleo extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,10 +35,10 @@ public class ServletEmpresa extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServletEmpresa</title>");            
+            out.println("<title>Servlet ServeltEmpleo</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ServletEmpresa at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ServeltEmpleo at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
